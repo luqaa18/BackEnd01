bdae21c7ae1152e75f0359ac0e8ef2b49fffdfff
luqaa18 <lumartinza321@gmail.com>
2024-11-12 20:26:32 -0300

    test branch

--- src/server.js
+++ src/server.js
@@ -2,17 +2,23 @@ import express from "express";
 import cartRouter from './routes/cart.router.js';
 import productRouter from "./routes/product.router.js";
 import path from 'path';
+import handlebars from 'express-handlebars';
 // import userRouter from "./routes/user.router.js";
 
 const app = express();
+
 
 //Middlewares
 app.use('/static', express.static(path.join(process.cwd(), "src", "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//handlebars
+app.engine('handlebars', handlebars.engine());
+app.set('views', path.join(process.cwd(), 'views'));
+app.set('view engine', 'handlebars');
+
 // app.use('/', userRouter);
 app.use('/api/products', productRouter);
 app.use('/api/carts', cartRouter);
-
 app.listen(8080, console.log("server ok en 8080"));
