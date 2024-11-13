db5853979c69c5ade69847481fa1818bec9c2900
luqaa18 <lumartinza321@gmail.com>
2024-11-13 16:39:40 -0300

    updated realtimeproducts

--- src/public/index.js
+++ src/public/index.js
@@ -1,1 +1,95 @@
 const clientSocket = io();
+
+const inputPrice = document.getElementById("price");
+const inputStock = document.getElementById("stock");
+const inputCategory = document.getElementById("category");
+const inputDescription = document.getElementById("description");
+const inputCode = document.getElementById("code");
+const inputTitle = document.getElementById("title");
+const btnAdd = document.getElementById("add");
+
+// Evento para agregar producto
+btnAdd.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (
+    !inputTitle.value ||
+    !inputDescription.value ||
+    !inputCategory.value ||
+    !inputCode.value ||
+    !inputPrice.value ||
+    !inputStock.value ||
+    isNaN(Number(inputPrice.value)) ||
+    isNaN(Number(inputStock.value))
+  ) {
+    alert("Asegurate de completar todos los campos!");
+    return;
+  }
+  const newProduct = {
+    title: inputTitle.value,
+    description: inputDescription.value,
+    category: inputCategory.value,
+    code: inputCode.value,
+    price: Number(inputPrice.value),
+    stock: Number(inputStock.value),
+  };
+  clientSocket.emit("addProd", newProduct);
+
+  // Limpia los campos una vez agregado el producto
+  inputTitle.value = "";
+  inputDescription.value = "";
+  inputCategory.value = "";
+  inputCode.value = "";
+  inputPrice.value = "";
+  inputStock.value = "";
+});
+
+// Escucha el evento de actualización de productos
+clientSocket.on("products", (products) => {
+  const productList = document.querySelector(".divContainer");
+  productList.innerHTML = "";
+  products.forEach((product) => {
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("borderBox");
+    productDiv.innerHTML = `
+        <div>
+          <p>Producto: ${product.title}</p>
+          <p>Código: ${product.code}</p>
+          <p>Descripción: ${product.description}</p>
+          <p>Precio: ${product.price}</p>
+          <p>Stock: ${product.stock}</p>
+          <p>Categoría: ${product.category}</p>
+        </div>
+        <button class="delete-button" data-id="${product.id}">Eliminar</button>
+      `;
+    productList.appendChild(productDiv);
+  });
+});
+
+// Maneja el clic en los botones de eliminación
+document.addEventListener("click", (event) => {
+  if (event.target.classList.contains("delete-button")) {
+    const productId = event.target.getAttribute("data-id");
+    clientSocket.emit("deleteProd", productId);
+  }
+});
+
+// Escucha el evento de actualización de productos después de eliminar
+clientSocket.on("updateProducts", (updatedProducts) => {
+  const productContainer = document.querySelector(".divContainer");
+  productContainer.innerHTML = ""; 
+
+  updatedProducts.forEach((product) => {
+    productContainer.innerHTML += `
+      <div class="borderBox" id="product-${product.id}">
+        <div>
+          <p>Producto: ${product.title}</p>
+          <p>Código: ${product.code}</p>
+          <p>Descripción: ${product.description}</p>
+          <p>Precio: ${product.price}</p>
+          <p>Stock: ${product.stock}</p>
+          <p>Categoría: ${product.category}</p>
+        </div>
+        <button class="delete-button" data-id="${product.id}">Eliminar</button>
+      </div>`;
+  });
+});
--- src/server.js
+++ src/server.js
@@ -1,28 +1,55 @@
-import express from "express";
-import cartRouter from "./routes/cart.router.js";
-import productRouter from "./routes/product.router.js";
-import viewsRouter from './routes/views.router.js';
 import path from "path";
+import express from "express";
+import { Server } from "socket.io";
 import handlebars from "express-handlebars";
+import cartRouter from "./routes/cart.router.js";
+import viewsRouter from "./routes/views.router.js";
+import productRouter from "./routes/product.router.js";
+import { prodMan } from "./managers/product.manager.js";
 // import userRouter from "./routes/user.router.js";
 
 const app = express();
 
 //Middlewares
-app.use(express.static(path.join(process.cwd(), "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(process.cwd(), "public")));
 
 //handlebars
+app.set("view engine", "handlebars");
 app.engine("handlebars", handlebars.engine());
 app.set("views", path.join(process.cwd(), "views"));
-app.set("view engine", "handlebars");
-
 
 // app.use('/', userRouter);
-app.use("/api/products", productRouter);
+app.use("/", viewsRouter);
 app.use("/api/carts", cartRouter);
-app.use('/', viewsRouter);
+app.use("/api/products", productRouter);
 
+const httpServer = app.listen(8080, console.log("server ok en 8080"));
 
-app.listen(8080, console.log("server ok en 8080"));
+const io = new Server (httpServer);
+
+io.on("connection", async (socket) => {
+    console.log("New Connection", socket.id);
+
+     // Emitir la lista de productos al cliente
+     const products = await prodMan.getAll();
+     socket.emit("products", products);
+ 
+     // Escuchar evento de agregar producto y guardarlo en el JSON
+     socket.on("addProd", async (productData) => {
+         const newProduct = await prodMan.create(productData);
+         const updatedProducts = await prodMan.getAll();
+         io.emit("products", updatedProducts);
+     });
+
+     socket.on("deleteProd", async (productId) => {
+        await prodMan.delete(productId);
+        const updatedProducts = await prodMan.getAll();
+        io.emit("updateProducts", updatedProducts);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected", socket.id);
+    });
+});
